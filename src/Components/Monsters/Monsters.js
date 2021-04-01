dc782648c258c6b8f73b971646dc7d3a8bede765
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-03-31 19:47:04 -0500

    added types and CR manually

--- src/Components/Monsters/Monsters.js
+++ src/Components/Monsters/Monsters.js
@@ -195,7 +195,40 @@ class Monsters extends React.Component {
             <label for="monsterTypeSearch">Search by Type:</label>
             <select className="form-control" id="monsterTypeSearch" onChange={this.updateTypeSearch}>
             <option value={null} >Pick a Type</option>
-              { monsterTypes.map((type) => <option value={type.monsterType}>{type.monsterType}</option>)}
+            <option >undead</option>
+            <option value={0.125}>1/8</option>
+              <option value={0.25}>1/4</option>
+              <option value={0.5}>1/2</option>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+              <option value={6}>6</option>
+              <option value={7}>7</option>
+              <option value={8}>8</option>
+              <option value={9}>9</option>
+              <option value={10}>10</option>
+              <option value={11}>11</option>
+              <option value={12}>12</option>
+              <option value={13}>13</option>
+              <option value={14}>14</option>
+              <option value={15}>15</option>
+              <option value={16}>16</option>
+              <option value={17}>17</option>
+              <option value={18}>18</option>
+              <option value={19}>19</option>
+              <option value={20}>20</option>
+              <option value={21}>21</option>
+              <option value={22}>22</option>
+              <option value={23}>23</option>
+              <option value={24}>24</option>
+              <option value={25}>25</option>
+              <option value={26}>26</option>
+              <option value={27}>27</option>
+              <option value={28}>28</option>
+              <option value={29}>29</option>
+              <option value={30}>30</option>
             </select>
             <button type="submit" onClick={this.searchByType} class="btn btn-primary">Search</button>
           </div>
